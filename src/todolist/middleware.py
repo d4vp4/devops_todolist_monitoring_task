b0ce5eaafe362cae794740fcf02a4c4719c70bc8
Danylo Povar <danila74povar@gmail.com>
2026-02-10 22:06:59 +0200

    monitoring_task1

--- src/todolist/middleware.py
+++ src/todolist/middleware.py
@@ -1,23 +1,17 @@
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
-from django.http import HttpResponse
-
-HTTP_REQUESTS_TOTAL = Counter(
-    'http_requests_total',
-    'Total number of HTTP requests',
-    ['method']
-)
+import time
+from prometheus_client import Counter, Gauge
 
 class PrometheusMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        # Лічильник запитів
+        self.request_counter = Counter('http_requests_total', 'Total HTTP Requests', ['method'])
+        # Метрика часу запуску (вимога ментора)
+        self.creation_time = Gauge('http_requests_total_created', 'Time when the counter was created')
+        self.creation_time.set(time.time())
 
     def __call__(self, request):
-        if request.path == '/metrics':
-            metrics_data = generate_latest()
-            return HttpResponse(metrics_data, content_type=CONTENT_TYPE_LATEST)
-
-        if request.method in ['GET', 'POST']:
-            HTTP_REQUESTS_TOTAL.labels(method=request.method).inc()
-
         response = self.get_response(request)
+        if request.path != '/metrics':
+            self.request_counter.labels(method=request.method).inc()
         return response
